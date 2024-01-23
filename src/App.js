928b4ce8cb1b4befa53e99e20d574e19494226d0
mary <noroozi.m90@gmail.com>
2024-01-23 15:04:45 -0500

    Routes added

--- src/App.js
+++ src/App.js
@@ -7,15 +7,25 @@ import {
   Contacts,
   Navbar,
 } from "./components";
+import { Navigate, Route, Routes } from "react-router";
 
 const App = () => {
   const [contacts, setContacts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const getContacts = () => {};
 
   return (
     <div className="App">
       <Navbar />
-      <Contacts contacts={contacts} loading={loading} />
+      <Routes>
+        <Route path="/" element={<Navigate to="/contacts" />} />
+        <Route
+          path="/contacts"
+          element={<Contacts contacts={getContacts} loading={loading} />}
+        />
+        <Route path="/contacts/:contactId" element={<ViewContacts />} />
+        <Route path="/contacts/edit/:contactId" element={<EditContacts />} />
+      </Routes>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 //Bootstrap
 // import "bootstrap/dist/css/bootstrap.min.css";
@@ -10,6 +11,8 @@ import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
