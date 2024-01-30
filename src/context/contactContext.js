8c596c653dcec6257bf4d2cf24a88060d11eddac
mary <noroozi.m90@gmail.com>
2024-01-29 23:19:21 -0500

    contactContext created

---
+++ src/context/contactContext.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export const contactContext = React.createContext({
+  loading: false,
+  setLoading: () => {},
+  contact: {},
+  setContacts: () => {},
+  contacts: [],
+  groups: [],
+  contactQuery: "",
+  filteredContacts: [],
+  searchContacts: () => {},
+  onContactChange: () => {},
+  createContactForm: () => {},
+  updateContactInfo: () => {},
+  removeContact: () => {},
+});
